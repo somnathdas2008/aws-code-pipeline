c3fa8121bd0e733f35965b1ae480029ac59eec44
SD <42784887+somnathdas2008@users.noreply.github.com>
2026-07-29 21:09:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-prod-sd"
     key            = "project/prod/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
   }
 }
