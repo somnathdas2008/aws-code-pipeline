586196bb45f25e96e58c87bed5fcdae10dd1a2ee
SD <42784887+somnathdas2008@users.noreply.github.com>
2026-07-29 21:29:57 +0530

    Update S3 bucket resources in main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ resource "aws_s3_bucket" "demo_bucket" {
   }
 }
 
-resource "aws_s3_bucket" "demo_bucket2" {
-  bucket = "somnath-demo-bucket-123456-test1"
+resource "aws_s3_bucket" "demo_bucket3" {
+  bucket = "somnath-demo-bucket-123456-test11"
 
   tags = {
     Environment = "test1"
